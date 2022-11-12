4d40f571bce5b9aea8d966723935f4f18aa57d65
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-12 10:21:14 -0300

    finalizando projeto no desktop

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import './style.css';
 
-const Details = () => {
+const Details = ({ mode }) => {
     const [country, setCountry] = useState([]);
     const [borderCountry, setBorderCountry] = useState([]);
     const { name } = useParams();
+    const language = [];
+    const currencies = [];
 
     useEffect(() => {
-        const Countries = async (name) => {
+        const countries = async (name) => {
             const url = `https://restcountries.com/v2/name/${name}`;
             const response = await fetch(url);
             const data = await response.json();
@@ -16,7 +19,7 @@ const Details = () => {
                 return findBorderCountries(border);
             })
         };
-        Countries(name);
+        countries(name);
 
         const findBorderCountries = async (border) => {
             const url = `https://restcountries.com/v2/alpha/${border}`;
@@ -26,17 +29,47 @@ const Details = () => {
         }
     }, [name]);
 
+    for (let i in country.languages) {
+        language.push(country.languages[i].name);
+    }
+
+    for (let i in country.currencies) {
+        currencies.push(country.currencies[i].name);
+    }
+
     return (
-        <>
-            <div id="goBack">
-                <Link to="/"><span>&larr;</span> Back</Link>
+        <div id='details'>
+            <div className="container">
+                <div id={`goBack-${mode}`}>
+                    <Link to="/"><i className="fa-solid fa-left-long"></i> Back</Link>
+                </div>
+                <div id='country'>
+                    <div id="img">
+                        <img src={`${country.flag}`} alt={`flag of ${country.name}`} />
+                    </div>
+                    <div className={`information-${mode}`}>
+                        <h2>{country.name}</h2>
+                        <div className="allInfo">
+                            <div id="left">
+                                <p>Native Name: <span>{country.nativeName}</span></p>
+                                <p>Population: <span>{country.population}</span></p>
+                                <p>Region: <span>{country.region}</span></p>
+                                <p>Sub Region: <span>{country.subregion}</span></p>
+                                <p>Capital: <span>{country.capital}</span></p>
+                            </div>
+                            <div id="right">
+                                <p>Top Level Domain: <span>{country.topLevelDomain}</span></p>
+                                <p>Currencies: <span>{currencies.join(', ')}</span></p>
+                                <p>Languages: <span>{language.join(', ')}</span></p>
+                            </div>
+                        </div>
+                        <div id="border">
+                            <p>Border Countries: <span>{borderCountry.join(', ')}</span></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div id='country'>
-                {/* <p>{country[0].region}</p> */}
-                <img src={`${country.flag}`} alt="" />
-                <h1>{country.borderCountries}</h1>
-            </div>
-        </>
+        </div>
     )
 }
 
